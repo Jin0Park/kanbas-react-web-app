b06df8bcd7a4eb05cf14178a62dd8a2fb6a714df
Dominic Park <dominicpark@Dominics-MacBook-Air.local>
2023-12-02 18:01:05 -0800

    update a6

--- src/project/users/client.js
+++ src/project/users/client.js
@@ -1,49 +1,58 @@
 import axios from "axios";
-const request = axios.create({
-  withCredentials: true,
-});
+// const request = axios.create({
+//   withCredentials: true,
+// });
 
 // export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const BASE_API = "https://kanbas-node-server-app-jfzp.onrender.com";
 console.log(BASE_API);
 export const USERS_API = `${BASE_API}/api/users`;
 export const signin = async (credentials) => {
-  console.log(request);
-  const response = await request.post( `${USERS_API}/signin`, credentials );
+  //console.log(request);
+  axios.defaults.withCredentials = true
+  const response = await axios.post( `${USERS_API}/signin`, credentials );
   return response.data;
 };
 export const account = async () => {
-    const response = await request.post(`${USERS_API}/account`);
+  axios.defaults.withCredentials = true
+    const response = await axios.post(`${USERS_API}/account`);
     return response.data;
 };
 export const updateUser = async (user) => {
-  const response = await request.put(`${USERS_API}/${user._id}`, user);
+  axios.defaults.withCredentials = true
+  const response = await axios.put(`${USERS_API}/${user._id}`, user);
   return response.data;
 };
 export const findAllUsers = async () => {
-  const response = await request.get(`${USERS_API}`);
+  axios.defaults.withCredentials = true
+  const response = await axios.get(`${USERS_API}`);
   return response.data;
 };
 export const createUser = async (user) => {
-  const response = await request.post(`${USERS_API}`, user);
+  axios.defaults.withCredentials = true
+  const response = await axios.post(`${USERS_API}`, user);
   return response.data;
 };
 export const findUserById = async (id) => {
-  const response = await request.get(`${USERS_API}/${id}`);
+  axios.defaults.withCredentials = true
+  const response = await axios.get(`${USERS_API}/${id}`);
   console.log(response);
   return response.data;
 };
 export const deleteUser = async (user) => {
-  const response = await request.delete(
+  axios.defaults.withCredentials = true
+  const response = await axios.delete(
     `${USERS_API}/${user._id}`);
   return response.data;
 };
 export const signup = async (credentials) => {
-  const response = await request.post(
+  axios.defaults.withCredentials = true
+  const response = await axios.post(
     `${USERS_API}/signup`, credentials);
   return response.data;
 };
 export const signout = async () => {
-  const response = await request.post(`${USERS_API}/signout`);
+  axios.defaults.withCredentials = true
+  const response = await axios.post(`${USERS_API}/signout`);
   return response.data;
 };
