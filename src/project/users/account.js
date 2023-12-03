36eef18d0058d1a4834b9977e718ae3266376b7a
Dominic Park <dominicpark@Dominics-MacBook-Air.local>
2023-12-02 17:39:36 -0800

    remove printouts

--- src/project/users/account.js
+++ src/project/users/account.js
@@ -40,10 +40,7 @@ function Account() {
 
   return (
     <div className="w-50">
-      <h1>{account.dob}</h1>
       <h1>Account {account}</h1>
-      <h1>{account.email}</h1>
-      console.log(account);
       {account && (
         <div className="input-container">
           <input value={account.password}
